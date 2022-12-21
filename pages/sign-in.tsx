7a3424ec78d762291c206ac73b0e902c18b7e9e6
Lutfi Azis <17lutfi21@gmail.com>
2022-12-21 18:49:46 +0700

    feat: sign-in page

---
+++ pages/sign-in.tsx
@@ -0,0 +1,54 @@
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormLabel,
+  Flex,
+  Spacer,
+  Input,
+  Link,
+  Stack,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import Head from "../components/Head";
+import Header from "../components/Header";
+
+export default function SignIn() {
+  return (
+    <>
+      <Head
+        title="Tebak Budaya"
+        description="Pemilihan mode permainan Tebak Budaya"
+      />
+      <Header />
+      <Container textAlign="center" pb={10}>
+        <Heading>Masuk ke Akunmu</Heading>
+        <Text>untuk pengalaman yang maksimal!</Text>
+      </Container>
+      <Container maxW="md">
+        <form>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>Alamat Email</FormLabel>
+              <Input type="email" placeholder="agus@gmail.com" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Kata Sandi</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Flex>
+              <Link color="blue.500">Lupa kata sandi?</Link>
+              <Spacer />
+              <Checkbox>Ingat saya</Checkbox>
+            </Flex>
+            <Button size="lg" width="100%" type="submit">
+              Masuk
+            </Button>
+          </Stack>
+        </form>
+      </Container>
+    </>
+  );
+}
