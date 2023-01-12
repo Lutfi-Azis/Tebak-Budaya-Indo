2b8f1446c1c617e46346713abc4ac63bf5a56a0a
Lutfi Azis <17lutfi21@gmail.com>
2023-01-12 10:40:14 +0700

    feat: add badges and correct answers to ReviewCard

--- components/Player/ReviewCard.tsx
+++ components/Player/ReviewCard.tsx
@@ -5,10 +5,21 @@ import {
   Image,
   Heading,
   useToken,
+  Tag,
+  TagLeftIcon,
+  TagLabel,
+  chakra,
 } from "@chakra-ui/react";
 import { FC } from "react";
 import Question from "../../types/Question";
 import { toTitleCase } from "../../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheck,
+  faXmark,
+  faClipboardList,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "./ReviewCard.module.css";
 
 type Props = { index: number; question: Question; answer: string };
 
@@ -18,21 +29,37 @@ const ReviewCard: FC<Props> = (props) => {
     isCorrect ? "green.300" : "red.300",
     isCorrect ? "green.800" : "red.800",
   ]);
-  const stylizedAnswer = toTitleCase(props.answer.replaceAll("-", " "));
+  const stylizedUserAnswer = toTitleCase(props.answer.replaceAll("-", " "));
+  const stylizedCorrectAnswer = toTitleCase(
+    props.question.answer.replaceAll("-", " ")
+  );
+  const Icon = chakra(FontAwesomeIcon);
 
   return (
-    <Card borderColor={border} borderWidth={3} cursor="pointer">
+    <Card borderColor={border} borderWidth={3}>
       <CardBody padding={0}>
         <Image
           src={process.env.NEXT_PUBLIC_HOST + props.question.imageSrc}
-          alt={`Gambar di ${stylizedAnswer}`}
+          alt={`Gambar di ${stylizedCorrectAnswer}`}
         />
       </CardBody>
+      <Tag colorScheme="gray" size="lg">
+        <TagLeftIcon>
+          <Icon icon={faClipboardList} />
+        </TagLeftIcon>
+        <TagLabel>{stylizedCorrectAnswer}</TagLabel>
+      </Tag>
       <CardFooter>
         <Heading color={heading} size="lg">
-          {props.index + 1}. {stylizedAnswer}
+          {props.index + 1}. {stylizedUserAnswer}
         </Heading>
       </CardFooter>
+      <Icon
+        bgColor={border}
+        color={heading}
+        className={styles.reviewBadge}
+        icon={isCorrect ? faCheck : faXmark}
+      />
     </Card>
   );
 };
