c1f65203a5cd28edcf738dacf0eb156e2253f80c
Lutfi Azis <17lutfi21@gmail.com>
2023-01-13 08:50:54 +0700

    feat: improve home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-import { Heading, useToken, Container, ButtonGroup } from "@chakra-ui/react";
+import {
+  Heading,
+  useToken,
+  Container,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  Text,
+  Image,
+  IconButton,
+  Icon,
+  Flex,
+  Spacer,
+  Box,
+} from "@chakra-ui/react";
 import Head from "../components/Head";
 import Header from "../components/Header";
 import Description from "../components/Description";
@@ -6,9 +20,13 @@ import styles from "./index.module.css";
 import ReviewCarousel from "../components/ReviewCarousel";
 import { REVIEWS } from "../constants";
 import ButtonLink from "../components/ButtonLink";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 export default function Home() {
-  const [primary800] = useToken("colors", ["primary.800"]);
+  const [heading, imgBorder] = useToken("colors", [
+    "primary.800",
+    "primary.300",
+  ]);
   return (
     <>
       <Head description="Halaman utama permainan Tebak Budaya" />
@@ -21,7 +39,7 @@ export default function Home() {
             fontFamily="Lobster"
             fontWeight={400}
             py={16}
-            color={primary800}
+            color={heading}
           >
             Tebak Budaya
           </Heading>
@@ -46,6 +64,82 @@ export default function Home() {
       <Container w={["100%", "80%", "50%"]} py={4}>
         <ReviewCarousel reviews={REVIEWS} />
       </Container>
+      <Grid templateColumns="repeat(12, 1fr)" gap={4} paddingTop={16}>
+        <GridItem colSpan={[12, 12, 8]} className={styles.centerify}>
+          <Container>
+            <Heading>Menjelajah Indonesia</Heading>
+            <Text>
+              Berkeliling seluruh Indonesia dari kenyamanan dan keamanan
+              kursimu. Temukan berbagai harta karun ilmu saat berpetualangan.
+            </Text>
+          </Container>
+        </GridItem>
+        <GridItem colStart={[2, 2, 9]} colEnd={12}>
+          <Image
+            src={
+              process.env.NEXT_PUBLIC_HOST +
+              "/assets/images/places/bali-temple.jpg"
+            }
+            alt="Sebuah bangunan tua di Bali"
+            borderColor={imgBorder}
+            className={styles.imageBit}
+          />
+        </GridItem>
+
+        <GridItem colStart={[2, 2, 2]} colEnd={[12, 12, 5]}>
+          <Image
+            src={
+              process.env.NEXT_PUBLIC_HOST +
+              "/assets/images/clothing/bali couple.jpg"
+            }
+            alt="Sebuah bangunan tua di Bali"
+            borderColor={imgBorder}
+            className={styles.imageBit}
+          />
+        </GridItem>
+        <GridItem colStart={[1, 1, 7]} colEnd={12} className={styles.centerify}>
+          <Container>
+            <Heading>Mempelajari Budaya</Heading>
+            <Text>
+              Pelajari budaya-budaya yang tersebar di nusantara melalui
+              gambar-gambar. Melatih mata Anda untuk mendeteksi detail-detail
+              kebudayaan kecil yang awalnya tidak kasat mata.
+            </Text>
+          </Container>
+        </GridItem>
+      </Grid>
+      <Container w={["90%", "70%", "40%"]} paddingY={12}>
+        <Flex>
+          <IconButton
+            aria-label={"YouTube link"}
+            icon={<Icon boxSize={16} as={FaYoutube} />}
+            variant="ghost"
+            colorScheme="secondary"
+          />
+          <Spacer />
+          <IconButton
+            aria-label={"Twitter link"}
+            icon={<Icon boxSize={16} as={FaTwitter} />}
+            variant="ghost"
+            colorScheme="secondary"
+          />
+          <Spacer />
+          <IconButton
+            aria-label={"Twitter link"}
+            icon={<Icon boxSize={16} as={FaInstagram} />}
+            variant="ghost"
+            colorScheme="secondary"
+          />
+        </Flex>
+      </Container>
+      <Box
+        as="footer"
+        textAlign="center"
+        height={32}
+        className={styles.centerify}
+      >
+        &#169; Lutfi Azis — 2023
+      </Box>
     </>
     // TODO: Add a leaderboard
   );
